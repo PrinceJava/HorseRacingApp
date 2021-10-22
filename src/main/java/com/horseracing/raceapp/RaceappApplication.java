5c132c82971cba8b3c48c346fdf23c848f573cd9
Raul Rosales <raurosales@paypal.com>
2021-10-22 13:40:32 -0700

    Created IntelliJ project

---
+++ src/main/java/com/horseracing/raceapp/RaceappApplication.java
@@ -0,0 +1,13 @@
+package com.horseracing.raceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaceappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/horseracing/raceapp/RaceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horseracing.raceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
