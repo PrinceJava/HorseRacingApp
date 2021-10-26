8df54a9994108cee23aa34c6117eb40df09ec102
Matthew James <matjames@paypal.com>
2021-10-25 22:00:31 -0500

    started race logic for hashtable

--- src/main/java/com/horseracing/raceapp/service/RaceServiceImpl.java
+++ src/main/java/com/horseracing/raceapp/service/RaceServiceImpl.java
@@ -1,20 +1,21 @@
 package com.horseracing.raceapp.service;
 
 import com.horseracing.raceapp.exception.InformationNotFoundException;
-import com.horseracing.raceapp.model.Horse;
-import com.horseracing.raceapp.model.Race;
-import com.horseracing.raceapp.model.Stable;
+import com.horseracing.raceapp.model.*;
 import com.horseracing.raceapp.model.forms.AddHorseToStableForm;
 import com.horseracing.raceapp.model.forms.RaceForm;
 import com.horseracing.raceapp.repository.HorseRepository;
 import com.horseracing.raceapp.repository.JockeyRepository;
 import com.horseracing.raceapp.repository.StableRepository;
+import com.horseracing.raceapp.repository.TrackRepository;
 import com.horseracing.raceapp.service.interfaces.HorseService;
 import com.horseracing.raceapp.service.interfaces.RaceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.w3c.dom.Node;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -32,15 +33,20 @@ public class RaceServiceImpl implements RaceService {
     @Autowired
     JockeyRepository jockeyRepository;
 
+    @Autowired
+    TrackRepository trackRepository;
+
 
     @Override
     public Race startRace(RaceForm form) {
 
         // TODO logic of taking horse and Jockey input, as well as Track input
-        List<Horse> horses = new ArrayList<>();
+        Hashtable<Horse, Jockey> racer = new Hashtable<>();
         try {
             Horse selectedHorse = horseRepository.findHorseByName(form.getHorseName());
-            horses.add(selectedHorse);
+            Jockey selectedJockey = jockeyRepository.findJockeyByName(form.getJockeyName());
+            Track selectedTrack = trackRepository.findTrackByName(form.getTrackName());
+            racer.put(selectedHorse,selectedJockey);
             return null;
         }catch(Exception e){
             throw new InformationNotFoundException("Something went wrong, please try again");
